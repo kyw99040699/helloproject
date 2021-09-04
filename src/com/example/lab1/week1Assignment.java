e5c822a983e174418815f2a5dafb6cddbaa3d33e
gim-yewon <gim-yewon@localhost>
2021-09-04 16:05:14 +0900

    assignment(week1)

---
+++ src/com/example/lab1/week1Assignment.java
@@ -0,0 +1,10 @@
+package com.example.lab1;
+
+public class week1Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is assignment for week1");
+	}
+
+}
